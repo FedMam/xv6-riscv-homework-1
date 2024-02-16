3e354096801649f5ae6337e3047b6d9c87866c42
FedMam <fedor_mamaev_v@mail.ru>
2024-02-16 22:01:34 +0300

    sum syscall part 3

--- kernel/sysadd.c
+++ kernel/sysadd.c
@@ -3,47 +3,12 @@
 #include "defs.h"
 
 uint64 sys_add(void) {
-    int num1 = 0, num2 = 0;
-    char input_ch = '\0';
+  int num1, num2;
 
-    for (int i = 0; i < 25; i++) {
-        printf("%d\n", 25);
-    }
+  argint(0, &num1);
+  argint(1, &num2);
 
-    /*
-    char buffer[25];
+  printf("%d\n", num1 + num2);
 
-    char *buffer_read_pos = buffer;
-    for (int i = 0; i < 25; i++) {
-        if (read(0, buffer_read_pos, 1) != 1) {
-        // failure
-        write(2, "sum: read failure\n", 23);
-        exit(1);
-        }
-
-        if (*buffer_read_pos == '\0' ||
-            *buffer_read_pos == '\n' ||
-            *buffer_read_pos == '\r')
-        break;
-
-        buffer_read_pos++;
-    }
-
-    char *ptr1 = buffer, *ptr2 = strchr(buffer, ' ');
-    
-    if (!ptr2) {
-        // the string does not contain a space
-        write(2, "sum: not enough args\n", 23);
-        exit(1);
-    }
-    
-    *ptr2++ = '\0'; // split string
-    
-    int num1 = atoi(ptr1), num2 = atoi(ptr2); // incorrect string converts to 0
-    int sum = num1 + num2;
-    
-    printf("%d\n", sum);
-    exit(0);
-    return 0;
-    */
+  return 0;
 }
--- user/add.c
+++ user/add.c
@@ -3,6 +3,26 @@
 
 int
 main(int argc, char *argv[]) {
-    add();
-    exit(0);
+  char buffer[25];
+   
+  if (!gets(buffer, 25)) {
+    // failure
+    write(2, "sum: read failure\n", 23);
+    exit(1);
+  }
+  
+  char *ptr1 = buffer, *ptr2 = strchr(buffer, ' ');
+  
+  if (!ptr2) {
+    // the string does not contain a space
+    write(2, "sum: not enough args\n", 23);
+    exit(1);
+  }
+  
+  *ptr2++ = '\0'; // split string
+  
+  int num1 = atoi(ptr1), num2 = atoi(ptr2); // incorrect string converts to 0
+  
+  add(num1, num2); // syscall
+  exit(0);
 }
